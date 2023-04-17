bfbeece503e2fbf4172623796e001cd2ab0e1be3
vinyeee <108808701+vinyeee@users.noreply.github.com>
2023-04-17 21:21:37 +0900

    [Silver V] Title: 소트인사이드, Time: 200 ms, Memory: 17656 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,24 +1,32 @@
+
 import java.util.Scanner;
- 
+
 public class Main {
+
 	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
- 
-		int[] counting = new int[10];
- 
-		int N = in.nextInt();
- 
-		while (N != 0) {
-			counting[N % 10]++;
-			N /= 10;
+		//자릿수 정렬
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		int A[] = new int[str.length()];
+		for(int i = 0; i<str.length();i++) {
+			A[i] = Integer.parseInt(str.substring(i,i+1));
 		}
- 
-		for (int i = 9; i >= 0; i--) {
-			while (counting[i]-- > 0) {
-				System.out.print(i);
+		// 선택 정렬
+		for(int i = 0; i<str.length(); i++) {
+			int Max = i; // 가장 첫 번째 원소
+			for (int j = i+1; j<str.length(); j++) {
+				if(A[j]>A[Max]) {
+					Max = j;	
+				}
+			}if(A[i] < A[Max]) {
+				int tmp = A[i];
+				A[i] = A[Max];
+				A[Max] = tmp; 
 			}
 		}
- 
+		for(int i = 0; i<A.length; i++) {
+			System.out.print(A[i]);
+		}
 	}
+
 }
