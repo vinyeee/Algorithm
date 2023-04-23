fc3afafda7a461721729e06a37e1d645d43ec2c8
vinyeee <108808701+vinyeee@users.noreply.github.com>
2023-04-23 18:50:55 +0900

    [Bronze V] Title: 오늘 날짜, Time: 192 ms, Memory: 17188 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		LocalDate dt = LocalDate.now();
+		System.out.println(dt);
+		
+			
+	}
+	
+
+}
