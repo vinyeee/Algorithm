739032d7166a10fd8b310e32661d31ce59c30b3c
vinyeee <108808701+vinyeee@users.noreply.github.com>
2024-02-12 00:10:45 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 328 ms, Memory: 46628 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -1,9 +1,17 @@
 import sys
-n=int(sys.stdin.readline())
-li=[]
-for i in range(n):
-    a, b = map(int, sys.stdin.readline().split())
-    li.append([a, b])
-li.sort()
-for i in li:
-    print(i[0], i[1])
+
+n = int(sys.stdin.readline())
+
+points = []
+
+for _ in range(n):
+    x,y = map(int, sys.stdin.readline().split())
+    points.append([x,y])
+
+
+points.sort()
+#print(points)
+
+
+for p in points:
+     print(f"{p[0]} {p[1]}")
