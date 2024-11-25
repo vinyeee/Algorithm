30d69532fa0e34e9aba7cc2998a0cb7fd6c02135
vinyeee <108808701+vinyeee@users.noreply.github.com>
2024-11-25 15:24:46 +0900

    Title: 되돌아오기, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 241125/되돌아오기/come-back.py
+++ 241125/되돌아오기/come-back.py
@@ -11,25 +11,32 @@ dx = [1,0,-1, 0]
 y,x = 0,0
 mapper = {'E':0,'S':1,'W':2,'N':3}
 
-    
-    
-def is_return_starting_point(n,y,x):
-    cnt = 0 #움직인 횟수 카운트
-    for _ in range(n):
-        d, l = input().split()
-        move_dir = mapper[d]
-        l = int(l)
-        while l > 0:
-            y,x = y + dy[move_dir], x + dx[move_dir]
-            cnt += 1
-            if y == 0 and x == 0:
-                print(cnt)
-                return True
-            l -= 1 # 한 칸 움직일 때마다 l에서 1씩 빼줌 
+ans = -1
+cnt = 0 #지금까지 걸린 시간
+
+        
+def move(move_dir,l):
+    global x,y
+    global ans,cnt
+
+    while l > 0: 
+        y, x = y + dy[move_dir], x + dx[move_dir]
+        cnt += 1
+        if y == 0 and x == 0:
+            ans = cnt
+            return True
+        l -= 1
     return False
 
 
-if is_return_starting_point(n,y,x):
-    pass
-else:
-    print(-1) 
+for _ in range(n):
+    d, l = input().split()
+    move_dir = mapper[d]
+    l = int(l)
+
+    done = move(move_dir,l)
+    if done:
+        break
+    
+print(ans)
+    
