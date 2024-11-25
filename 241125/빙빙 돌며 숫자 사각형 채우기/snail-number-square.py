1a9c0ee45cc6a999c9a4759f57c2f2ac31103f62
vinyeee <108808701+vinyeee@users.noreply.github.com>
2024-11-25 12:17:48 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 241125/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
+++ 241125/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
@@ -17,10 +17,11 @@ graph[y][x] = 1
 
 #구현 
 def in_range(ny,nx):
-    return 0<= nx < n and 0 <= ny < m
+    return 0<= ny < n and 0 <= nx < m
 
 for i in range(2, n * m + 1): #i 값으로 채울거임
     ny, nx = y + dys[move_dir], x + dxs[move_dir]
+    
     if not in_range(ny,nx) or graph[ny][nx] != 0:
         #격자 바깥이거나 이미 방문했던 곳이라면 90도 회전 
         move_dir = ( move_dir + 1 ) % 4
@@ -28,6 +29,7 @@ for i in range(2, n * m + 1): #i 값으로 채울거임
     #한 후 그 다음 위치로 이동한 다음 배열에 값을 채워 넣음 
     y, x = y + dys[move_dir], x + dxs[move_dir]
     graph[y][x] = i
+    
 
 
 
