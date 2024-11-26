4aea2b2109acb0924c1410425b37ee60458a37ba
vinyeee <108808701+vinyeee@users.noreply.github.com>
2024-11-26 15:12:30 +0900

    Title: 최소공배수 구하기, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 241126/최소공배수 구하기/find-the-least-common-multiple.py
+++ 241126/최소공배수 구하기/find-the-least-common-multiple.py
@@ -4,7 +4,7 @@ n,m = map(int,input().split())
 
 def find_gcd(n,m):
     gcd = 1
-    for i in range(2, min(n,m)):
+    for i in range(2, min(n,m)+1):
         if n % i == 0 and m % i == 0:
             gcd = i
     return gcd
@@ -16,7 +16,6 @@ def find_lcm(n,m):
     b = m // x
     lcm = x * a * b 
     return lcm
-
 
 
 print(find_lcm(n,m))
