e67e40b0859e7a4a6538f346d934045958e342ed
vinyeee <108808701+vinyeee@users.noreply.github.com>
2024-11-27 14:13:04 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 81ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241127/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -0,0 +1,23 @@
+n1, n2 = map(int, input().split())
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+
+def is_part_series(a,b):
+    idx = a.index(b[0])
+    if idx is None:
+        return "No"
+    b_idx = 0
+    for i in range(idx,idx + len(b)):
+        if a[i] != b[b_idx]:
+            return "No"
+        b_idx += 1
+    return "Yes"
+
+
+if n1 > n2:
+    print(is_part_series(a, b))
+else:
+    print(is_part_series(b, a))
+
