8b2040447e96570d43a2815fd03aff4091ccb18a
vinyeee <108808701+vinyeee@users.noreply.github.com>
2024-11-27 15:12:52 +0900

    Title: 2021년 날짜의 유무, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241127/2021년 날짜의 유무/with-or-without-2021.py
@@ -0,0 +1,28 @@
+m,d = map(int, input().split())
+
+
+def is_valid_day(m,d):
+    if m == 2 and ( d < 1 and d > 28):
+        return False
+    if m == 8 and ( d < 1 and d > 31):
+        return False
+    if m % 2 == 0 and ( d < 1 and d > 30):
+        return False
+    if m % 2 != 0 and ( d < 1 and d > 31):
+        return False
+    return True
+
+
+
+def is_valid_month(m,d):
+
+    if m < 1 or m > 12:
+        return False
+    if not is_valid_day(m,d):
+        return False
+    return True
+
+if is_valid_month(m,d):
+    print("Yes")
+else:
+    print("No")
