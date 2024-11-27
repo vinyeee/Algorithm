554cd185e9fbb4ef5a66df98583a00008d1ffbc5
vinyeee <108808701+vinyeee@users.noreply.github.com>
2024-11-27 11:45:24 +0900

    Title: 사칙연산 함수, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241127/사칙연산 함수/quadratic-operations-function.py
+++ 241127/사칙연산 함수/quadratic-operations-function.py
@@ -16,15 +16,26 @@ def divide(a,b):
     return int(a / b)
 
 
-if o == "+":
-    x = add(a,b)
-elif o == "-":
-    x = minus(a,b)
-elif o == "*":
-    x = multiple(a,b)
-elif o == "/":
-    x = divide(a,b)
-else:
-    x = False
 
-print(f"{a} {o} {b} = {x}")
+
+
+
+def print_expression(a,o,b):
+
+    if o not in ["+", "-", "*", "/"]:
+        return False
+
+    if o == "+":
+        x = add(a,b)
+    elif o == "-":
+        x = minus(a,b)
+    elif o == "*":
+        x = multiple(a,b)
+    elif o == "/":
+        x = divide(a,b)
+
+    print(f"{a} {o} {b} = {x}")
+
+
+
+print_expression(a,o,b)
