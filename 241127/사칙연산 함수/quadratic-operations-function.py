353c18ce4a96acc695b8d208ed94a30046909fa1
vinyeee <108808701+vinyeee@users.noreply.github.com>
2024-11-27 11:35:10 +0900

    Title: 사칙연산 함수, Time: 77ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241127/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,30 @@
+a,o,b = input().split()
+a = int(a)
+b = int(b)
+
+def add(a,b):
+    return a + b 
+
+def minus(a,b):
+    return a - b
+
+def multiple(a,b):
+    return a * b
+
+
+def divide(a,b):
+    return int(a / b)
+
+
+if o == "+":
+    x = add(a,b)
+elif o == "-":
+    x = minus(a.b)
+elif o == "*":
+    x = multiple(a,b)
+elif o == "/":
+    x = divide(a,b)
+else:
+    x = False
+
+print(f"{a} {o} {b} = {x}")
