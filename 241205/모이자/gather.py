2664d52429803cf2588ec00d6010b62e67525bc8
vinyeee <108808701+vinyeee@users.noreply.github.com>
2024-12-05 18:27:00 +0900

    Title: 모이자, Time: 52ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241205/모이자/gather.py
@@ -0,0 +1,26 @@
+import sys
+
+INT_MAX = sys.maxsize
+
+n = int(input())
+house = list(map(int, input().split()))
+
+
+
+#1 2 3 2 6
+
+min_distance = INT_MAX
+
+for i in range(n):
+    start = i #i번째 집에 모인다고 가정했을 때 
+
+    move_distance = 0
+    for j in range(n):
+        move_distance += house[j] * abs(i - j) #j번째 집에 사는 사람 수 - |시작점 - 현재집위치|
+
+    min_distance = min(min_distance, move_distance)
+
+print(min_distance)
+
+
+
