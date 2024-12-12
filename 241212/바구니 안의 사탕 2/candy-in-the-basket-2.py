d0eccb73df953601937f259ef5b7e9a41d8e11c2
YeBin Hwang <108808701+vinyeee@users.noreply.github.com>
2024-12-12 18:55:32 +0900

    Title: 바구니 안의 사탕 2, Time: 52ms, Memory: 16MB, Status: Passed - CodeTree

--- 241212/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 241212/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,16 +1,17 @@
 n,k = map(int, input().split())
 
-placed = [0] * (100000)
+placed = [0] * (100 + 1)
 
 for _ in range(n):
     candy, idx = map(int, input().split())
     placed[idx] += candy
 
 max_sum = 0
-for i in range(k, 100000 - k): # 중심점 c
+for i in range(0,101): # 중심점 c
     internal_sum = 0
     for j in range(i - k, i + k + 1):
-        internal_sum += placed[j]
+        if j >= 0 and j <= 100:
+            internal_sum += placed[j]
     max_sum = max(max_sum, internal_sum)
 
 print(max_sum)
