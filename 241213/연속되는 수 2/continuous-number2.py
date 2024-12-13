16e407f30f6412eb60e22b9e634272abe3e1c63b
YeBin Hwang <108808701+vinyeee@users.noreply.github.com>
2024-12-13 15:29:12 +0900

    Title: 연속되는 수 2, Time: 64ms, Memory: 16MB, Status: Passed - CodeTree

--- 241213/연속되는 수 2/continuous-number2.py
+++ 241213/연속되는 수 2/continuous-number2.py
@@ -2,15 +2,14 @@ n = int(input())
 
 arr = [int(input()) for _ in range(n)]
 
+cnt = 0
 max_len = 0
 for i in range(n):
-    if i == 0 or arr[i] != arr[i-1]:
+
+    if i != 0 and arr[i] == arr[i-1]:
+        cnt += 1
+    else: 
         cnt = 1
-        for j in range(i,n-1):
-            if arr[j] == arr[j+1]:
-                cnt += 1
-            else:
-                break
-    max_len = max(max_len , cnt)
+    max_len = max(cnt , max_len)
 
 print(max_len)
