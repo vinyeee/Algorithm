13b7d4d5b27f90cd81bf1f4f8da00687c68e4908
YeBin Hwang <108808701+vinyeee@users.noreply.github.com>
2024-12-15 22:14:23 +0900

    Title: 진수 to 진수, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241215/진수 to 진수/transformation-of-number-system.py
@@ -0,0 +1,37 @@
+a, b = map(int, input().split())
+n = list(map(int, input()))
+
+#a진수로 표현된 n을 10진수로 
+
+#10진수로 표현된 n을 다시 b진수로 변환 
+
+
+def decimal_to_b(decimal):
+
+    digits = []
+
+    while True:
+
+        if decimal < b:
+            digits.append(str(decimal))
+            break
+
+        digits.append(str(decimal % b))
+        decimal //= b
+
+    num = "".join(digits)
+    return num
+
+
+
+def a_to_decimal(n):
+
+    num = 0
+    for i in range(len(n)):
+        num += n[i] * (a ** (len(n) - 1 - i))
+    return num 
+
+
+decimal = a_to_decimal(n)
+num = decimal_to_b(decimal)
+print(num)
