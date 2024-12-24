5863b740d122c608ba604979cbbc37be3c3d5317
YeBin Hwang <108808701+vinyeee@users.noreply.github.com>
2024-12-24 17:46:52 +0900

    Title: 빙산의 일각 2, Time: 55ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 241224/빙산의 일각 2/the-tip-of-the-iceberg-2.py
@@ -0,0 +1,17 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+
+max_num = 0
+for i in range(1, max(arr)):
+    copy_arr = arr[:]
+    for j in range(n):
+        copy_arr[j] -= i
+    num =len(''.join(list(map(str,copy_arr))).split('0'))
+    max_num = max(max_num, num)
+print(max_num)
+
+
+
+
+    
+    
