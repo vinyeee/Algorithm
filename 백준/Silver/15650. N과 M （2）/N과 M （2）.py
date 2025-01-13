1333fb12c54684baae7197d8cba72bf1c5845e73
YeBin Hwang <108808701+vinyeee@users.noreply.github.com>
2025-01-13 21:11:32 +0900

    [Silver III] Title: N과 M (2), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,34 +1,25 @@
-'''
-1. 알고리즘
-- 백트래킹에다가 오름차순 판단 집어넣기
-2. 시간복잡도
-- 중복이 없으니까 n은 최대 10까지 가능 
-3. 자료구조
-- 방문체크 boolean배열 
-- 결과를 저장할 int 배열 
-'''
-
 import sys
-
 input = sys.stdin.readline
-n,m = map(int, input().split())
 
-result = []
+n,m = map(int, input().strip().split()) 
 visited = [False] * (n + 1)
-def recur(start):
-    if start == m + 1:
-        print(' '.join(map(str, result)))
+result = []
+
+def recursion(cnt):
+    if cnt == m:
+        print(" ".join(list(map(str, result))))
         return 
     
-    for i in range(start, n+1): #방문 하지 않았고 리스트 맨 마지막에 있는 것보다 크면 담음 
-        if not visited[i] and (len(result) == 0 or result[-1] < i ) :
-            
-            visited[i] = True
-            result.append(i)
-            recur(start + 1)
-            visited[i] = False
-            result.pop()
+    for i in range(1,n+1):
+        if not visited[i]:
+            if len(result) == 0 or (len(result) >0 and i > result[-1]):
+                visited[i] = True
+
+                result.append(i)
+                recursion(cnt + 1)
+
+                visited[i] = False
+                result.pop()
 
 
-recur(1)
-
+recursion(0)
