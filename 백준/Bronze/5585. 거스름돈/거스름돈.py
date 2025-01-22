2b6c6f0b76979ededc4fc5809c1dacfa0bd60c44
YeBin Hwang <108808701+vinyeee@users.noreply.github.com>
2025-01-22 18:06:46 +0900

    [Bronze II] Title: 거스름돈, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.py
+++ 백준/Bronze/5585. 거스름돈/거스름돈.py
@@ -2,34 +2,13 @@ import sys
 input = sys.stdin.readline
 
 n = int(input().strip())
+n = 1000 - n
+coins = [500, 100, 50, 10, 5, 1]
 
-cnt = 0
-
-
-n = 1000 - n 
-
-if n >= 500:
-    cnt += n // 500
-    n %= 500
-
-if n >= 100:
-    cnt += n // 100
-    n %= 100
-
-if n >= 50:
-    cnt += n // 50
-    n %= 50
-
-if n >= 10:
-    cnt += n // 10
-    n %= 10
-
-if n >= 5:
-    cnt += n // 5
-    n %= 5
-
-if n >= 1:
-    cnt += n // 1
-    n %= 1
+cnt = 0 
+for coin in coins:
+    if n >= coin:
+        cnt += n // coin
+        n %= coin
 
 print(cnt)
