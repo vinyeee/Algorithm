073990cc6d5722f752dfe24dd87b8948b7076a76
YeBin Hwang <108808701+vinyeee@users.noreply.github.com>
2025-01-24 21:54:27 +0900

    [Silver IV] Title: 동전 0, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,21 +1,15 @@
 import sys
-#input = sys.stdin.readline()
+input = sys.stdin.readline
 
-n , k = map(int, input().strip().split())
+n, k = map(int, input().strip().split())
 
-coin = [int(input()) for _ in range(n)]
-coin.sort(reverse=True)
+coins = [int(input().strip()) for _ in range(n)]
+coins.sort(reverse = True)
 
-i = 0
-answer = 0
-while k > 0:
-    p = k // coin[i]
-    if p == 0:
-        i += 1
-        continue
-    answer += p
-    k -= coin[i] * p
-    i += 1
+cnt = 0
+for coin in coins:
+    if k >= coin and k > 0:
+        cnt += (k // coin)
+        k %= coin 
 
-
-print(answer)
+print(cnt)
