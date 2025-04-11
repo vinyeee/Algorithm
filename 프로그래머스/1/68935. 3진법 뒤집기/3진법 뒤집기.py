266e2adb4696a603dc8f4707da880854e4025989
YeBin Hwang <108808701+vinyeee@users.noreply.github.com>
2025-04-11 13:54:54 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 9.43 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,21 +1,12 @@
 #n을 3진법 상으로 표현 -> 3진법 뒤집음 -> 다시 10진법으로 표현
-def convert_to_decimal(n): #문자
-    result = 0
-    for i in range(len(n)):
-        result += int(n[-1 - i]) * (3**i)  
-        
-    return result
 
 def decimal_to_3(n):
-    result = []
-    while n // 3 != 0:
-        result.append(str(n % 3))
+    tmp = ''
+    while n :
+        tmp += str(n % 3)
         n = n // 3
-    result.append(str(n))
-    return "".join(result)
+    return tmp
 
 def solution(n): 
-    
-    answer = 0
     answer = decimal_to_3(n)
-    return convert_to_decimal(answer)
+    return int(answer, 3)
