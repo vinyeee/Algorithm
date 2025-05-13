9a01cfbca4c546e3f87b77d7c4d1dc82db253eed
YeBin Hwang <108808701+vinyeee@users.noreply.github.com>
2025-05-13 23:59:32 +0900

    Title: 최대 증가 부분 수열, Time: 63ms, Memory: 17MB, Status: Passed - Codetree

--- 250513/최대 증가 부분 수열/longest-increasing-subsequence.py
+++ 250513/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -4,22 +4,12 @@ m = list(map(int, input().split()))
 
 # Please write your code here.
 INT_MIN = - sys.maxsize
-dp = [0] * n
+dp = [1] * n
 
-def init():
-
-    for i in range(n):
-        dp[i] = INT_MIN 
-    dp[0] = 1
-
-init()
 
 for i in range(1,n):
     for j in range(0,i):
 
-        if dp[j] == INT_MIN:
-            continue
-            
         if m[i] > m[j]:
             dp[i] = max(dp[i], dp[j] + 1)
 
