03128a12fa64ed2ae26ceba6e0e06f4b5e9acecd
YeBin Hwang <108808701+vinyeee@users.noreply.github.com>
2025-05-17 18:45:22 +0900

    Title: 달에서 무게 구하기, Time: 5ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250517/달에서 무게 구하기/weight-on-the-moon.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a = 13;
+    double rate = 0.165;
+    
+    printf("%d * %.6lf = %.6lf", a, rate, a * rate);
+    return 0;
+}
