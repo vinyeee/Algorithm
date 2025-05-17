ba87390bd426a868667eddcec9f8a62441484192
YeBin Hwang <108808701+vinyeee@users.noreply.github.com>
2025-05-17 18:59:09 +0900

    Title: 길이 단위 변환하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250517/길이 단위 변환하기/change-length-unit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    double ft = 30.48;
+    long mile = 160934;
+
+    printf("%.1fft = %.1fcm\n", 9.2, 9.2 * ft);
+    printf("%.1fmi = %.1fcm", 1.3, 1.3 * mile);
+    return 0;
+}
