062cf0c5411c49e20b9a357f31c22b9a3031bc82
YeBin Hwang <108808701+vinyeee@users.noreply.github.com>
2025-05-17 19:17:31 +0900

    Title: 정수 복사, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250517/정수 복사/copy-integer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a = 3;
+    int b = 4;
+
+    b = a;
+
+    printf("%d %d\n",a,b);
+    printf("%d", a*b);
+    return 0;
+}
