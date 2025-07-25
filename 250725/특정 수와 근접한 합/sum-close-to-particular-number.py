eb33742d01e4430bdb81c705ba2bd9bb62945de3
YeBin Hwang <108808701+vinyeee@users.noreply.github.com>
2025-07-25 22:50:53 +0900

    Title: 특정 수와 근접한 합, Time: 77ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250725/특정 수와 근접한 합/sum-close-to-particular-number.py
+++ 250725/특정 수와 근접한 합/sum-close-to-particular-number.py
@@ -7,12 +7,13 @@ arr = list(map(int, input().split()))
 # 구해야 하는 것: S값과 S에 가장 가까운 s값 
 # => b값이 최소가 되어야 함 
 
-b = sys.maxsize
+min_diff = sys.maxsize
 
 for i in range(N):
     for j in range(i+1, N):
         s = sum(arr) - arr[i] - arr[j] #두 수를 제외한 나머지를 다 더한 값 
-        b = min(b, abs(S - s)) # S값과 s값의 차
+        diff = abs(S-s)
+        min_diff = min(min_diff, diff) # S값과 s값의 차
 
 print(b) 
         
