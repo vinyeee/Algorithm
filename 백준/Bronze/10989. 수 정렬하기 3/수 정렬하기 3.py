d3b12a947dedfa7ea1a43ceedd19a013860aa218
YeBin Hwang <108808701+vinyeee@users.noreply.github.com>
2025-08-17 00:19:45 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 8292 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -2,13 +2,11 @@ import sys
 input = sys.stdin.readline
 
 n = int(input().strip())
-count = [0] * 10001  # 1~10000 정수라 가정
+cnt = [0] * 10001
 
-for _ in range(n):
-    num = int(input().strip())
-    count[num] += 1
+for i in range(n):
+    cnt[int(input().strip())] += 1
 
-for i in range(10001):
-    for _ in range(count[i]):
-        print(i)
- 
+for i in range(1,len(cnt)):
+    for _ in range(cnt[i]):
+        print(i)     
